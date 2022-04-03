aa327b267b894c8b276b3cf57935c08b4fd71ef9
youssef <youssef.abdelmaksoud@ucalgary.ca>
2022-04-03 12:58:04 -0600

    refresh

--- src/Home.js
+++ src/Home.js
@@ -50,7 +50,7 @@ class Home extends Component {
   componentDidMount(){
     this.getAllTrips();
     this.getAllPatients();
-    this.startInterval();
+    this.viewTripProgress();
   }
 
   stopInterval(){
