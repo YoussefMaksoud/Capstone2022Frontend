c4d9ec5d5ad54466cacff2c5cf23feb8b0b45b1c
youssef <youssef.abdelmaksoud@ucalgary.ca>
2022-04-04 19:36:49 -0600

    FAQ adjusted for fair

--- src/HomescreenComp/QandA.js
+++ src/HomescreenComp/QandA.js
@@ -1,26 +1,26 @@
 export const questions = [
     {
-        question:"In a website browser address bar, what does “www” stand for?",
-        answer:"World Wide Web"
+        question:"Why was heroku used?",
+        answer:"Heroku allows for very rapid and continuous application deployment, a codebase on github is used and deployed to a heroku URL."
     },
     {
-        question:"What was the first toy to be advertised on television?",
-        answer:"Mr. Potato Head"
+        question:"Why use react?",
+        answer:"React is responsive and allows for HTML components to be reused making it very easy for developers to quickly implement a user interface."
     },
     {
-        question:"In the United Kingdom, what is the day after Christmas known as?",
-        answer:"Boxing Day"
+        question:"Advantages and disadvantages of using a RESTful API?",
+        answer:"Restful API is nice for web development, it allows endpoints to be used on a backend which allows for functionality to be seperated (reduces coupling), bad for live data transmission"
     },
     {
-        question:"What is the name of the Earth’s largest ocean?",
-        answer:"The Pacific Ocean"
+        question:"What are some of the difficulties encountered?",
+        answer:"Making data display relatively live. Data could not be transmitted directly to user, it has to go through the backend, to the database, then get pulled by the frontend"
     },
     {
-        question:"In what year was the first-ever Wimbledon Championship held?",
-        answer:"1877"
+        question:"Why was spring boot used?",
+        answer:"Spring boot allows for very rapid rest api implementation, and integrates very nicely with react and MySQL."
     },
     {
-        question:"What does BMW stand for (in English)?",
-        answer:"Bavarian Motor Works"
+        question:"How do the operators know the patient ID and healthcare number?",
+        answer:"Ensuring that health records are correct was outside the scope of the project, but in the future, the device can prompt the user for an input."
     },
 ]
